ff945b1059f04fcec11b85af08cc05a35e29df98
tavareskerol <tavareskerolayne6@gmail.com>
2025-09-17 17:25:54 -0300

    Subindo atividade

---
+++ atividade_string.py
@@ -0,0 +1,62 @@
+#questao 1
+'''
+nome = "tavares"
+print(nome[1])
+'''
+#questao 2
+'''
+nome = "tavares"
+print(nome[6])
+''' 
+#questao 3
+'''
+nome = "tavares" 
+print(nome[2])
+'''
+#questao 4
+'''
+nome = "tavares"
+print(nome[5])
+'''
+#questao 5
+'''
+nome = "tavares"
+print(nome[0])
+print(nome[1])
+print(nome[2])
+print(nome[3])
+print(nome[4])
+print(nome[5])
+print(nome[6])
+'''
+#questao 6
+'''
+nome = input("digite seu nome:")
+encontrar = input("digite o que deseja encontrar:")
+if encontrar in nome:
+    print(f'{encontrar} esta em {nome}')
+else:
+    print(f'{encontrar} nao esta em {nome}')
+    '''
+#questao 7
+'''
+nome = input("digite seu nome")
+encontrar = input("digite uma letra")
+if encontrar not in nome:
+    print(f'{encontrar} nao esta em {nome}')
+else:
+    print(f'{encontrar} esta em nome') 
+    '''
+#questao 8 
+'''
+nome = input("digite seu nome")
+encontrar = "res"
+if encontrar in nome
+    print("esta no nome tavares")
+else:
+    print("nao esta no nome tavares") 
+    '''
+#questao 9
+
+#questao 10
+
